cd026d1e29fd9cf2bcf8ff7e753f72a2dd92ad6d
Anderson Rocha <anderson.rocha@hotmail.com>
2018-11-02 17:39:37 -0300

    Insert
    Análise de dados de produção de Petróleo (Dados do GAPMINDER)

---
+++ Oil_Production_Analysis.R
@@ -0,0 +1,31 @@
+getwd()
+setwd('C:\\Users\\Anderson\\Documents\\UDACITY\\Nanodegree Data Scientist\\Aulas de R\\Gap_Minder\\Gap_Minder\\')
+ds <- read.csv('oil_production_total.csv')
+names(ds)
+
+#IMPORTANDO A BIBLIOTECA PARA TRATAMENTO DOS DADOS
+library(tidyr)
+library(ggplot2)
+
+#FAZENDO A TRANSFORMAÇÃO DAS COLUNAS PARA LINHAS
+ds_t <- gather(ds,Year,Production,-geo,na.rm = FALSE,convert = TRUE)
+#TRATANDO A COLUNA ANO, EXTRAINDO SÓ OS NÚMEROS E IGNORANDO O "X"
+ds_t$Year <-substr(ds_t$Year, 2,5)
+#VERIFICANDO O TIPO DE DADOS DA COLUNA ANO
+typeof(ds_t$Year)
+#CONVERTENDO PARA NUMÉRICO
+ds_t$Year <- as.numeric(as.character(ds_t$Year))
+#OS DADOS BRUTOS ESTÃO EM Tons Of Oil Equivalent (TOE)
+#OS MESMOS SERÃO CONVERTIDOS PARA Barrels Of Oil Equivalent (BBOE)
+# 1 (TOE) = 6.8412 (BBOE)
+ds_t$Production <- ds_t$Production*6.8412
+
+
+#PRODUÇÃO DO BRAZIL AO LONGO DOS ANOS
+ggplot(data = subset(ds_t,ds_t=='Brazil'), aes(x = Year, y = Production/1000000))+
+  geom_line(color = "#00AFBB", size = 2)+
+  ylab("Produção em milhões de BBOE (Barris de Óleo Equivalente)")+
+  xlab("Ano")
+
+
+
